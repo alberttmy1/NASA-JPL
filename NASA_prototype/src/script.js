e17593aee36b02995e2008572b4271dd43aeacf5
samuel-mast <84589616+samuel-mast@users.noreply.github.com>
2023-02-03 16:40:57 -0700

    Add files via upload
    Prototype Start

---
+++ NASA_prototype/src/script.js
@@ -0,0 +1,104 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import SceneInit from "./lib/SceneInit";
+import Planet from "./lib/Planet";
+import Rotation from "./lib/Rotation";
+
+//app.use('/assets', express.static(path.join(__dirname, './assets')));
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+//const scene = new THREE.Scene()
+let test = new SceneInit();
+test.initScene();
+test.animate();
+
+// Objects
+
+// Radius,Hight, width
+const sunGeometry = new THREE.SphereGeometry(8);
+//TODO fix these shitty textures
+const sunTexture = new THREE.TextureLoader().load('./sun.jpeg');
+
+// const sunGeometry = new THREE.BoxGeometry(8,8,8)
+
+// Materials
+
+// const sunMaterial = new THREE.MeshStandardMaterial();
+const sunMaterial = new THREE.MeshBasicMaterial({map: sunTexture});
+// const sunMaterial = new THREE.MeshBasicMaterial();
+// sunMaterial.color = new THREE.Color(0xff0000);
+
+
+// Mesh
+
+// const sunMesh = new THREE.Mesh(sunGeometry,sunMaterial)
+const sunMesh = new THREE.Mesh(sunGeometry,sunMaterial);
+const solarSystem = new THREE.Group();
+solarSystem.add(sunMesh);
+test.scene.add(solarSystem);
+
+
+//planets
+const mercury = new Planet(2, 16, "mercury.png");
+const mercuryMesh = mercury.getMesh();
+let mercurySystem = new THREE.Group();
+mercurySystem.add(mercuryMesh);
+
+const venus = new Planet(3, 32, "venus.jpeg");
+const venusMesh = venus.getMesh();
+let venusSystem = new THREE.Group();
+venusSystem.add(venusMesh);
+
+const earth = new Planet(4, 48, "earth.jpeg");
+const earthMesh = earth.getMesh();
+let earthSystem = new THREE.Group();
+earthSystem.add(earthMesh);
+
+const mars = new Planet(3, 64, "mars.jpeg");
+const marsMesh = mars.getMesh();
+let marsSystem = new THREE.Group();
+marsSystem.add(marsMesh);
+//add all planaets to solarsystem
+solarSystem.add(mercurySystem,venusSystem, earthSystem, marsSystem);
+//solarSystem.add(mercurySystem);
+
+const mercuryRotation = new Rotation(mercuryMesh);
+const mercuryRotationMesh = mercuryRotation.getMesh();
+mercurySystem.add(mercuryRotationMesh);
+const venusRotation = new Rotation(venusMesh);
+const venusRotationMesh = venusRotation.getMesh();
+venusSystem.add(venusRotationMesh);
+const earthRotation = new Rotation(earthMesh);
+const earthRotationMesh = earthRotation.getMesh();
+earthSystem.add(earthRotationMesh);
+const marsRotation = new Rotation(marsMesh);
+const marsRotationMesh = marsRotation.getMesh();
+marsSystem.add(marsRotationMesh);
+
+// NOTE: Add solar system mesh GUI.
+//await initGui();
+const solarSystemGui = gui.addFolder("solar system");
+solarSystemGui.add(mercuryRotationMesh, "visible").name("mercury").listen();
+solarSystemGui.add(venusRotationMesh, "visible").name("venus").listen();
+solarSystemGui.add(earthRotationMesh, "visible").name("earth").listen();
+solarSystemGui.add(marsRotationMesh, "visible").name("mars").listen();
+
+// NOTE: Animate solar system at 60fps.
+const EARTH_YEAR = 2 * Math.PI * (1 / 60) * (1 / 60);
+const animate = () => {
+  sunMesh.rotation.y += 0.001;
+  mercurySystem.rotation.y += EARTH_YEAR * 4;
+  venusSystem.rotation.y += EARTH_YEAR * 2;
+  earthSystem.rotation.y += EARTH_YEAR;
+  marsSystem.rotation.y += EARTH_YEAR * 0.5;
+  requestAnimationFrame(animate);
+};
+animate();
