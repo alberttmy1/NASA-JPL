6546a3e35fda586b36fb40822dd4991cad4d8a60
sama9843 <sama9843@colorado.edu>
2023-02-06 17:07:15 -0700

    Reverted Module Changes

--- NASA_prototype/src/lib/SceneInit.js
+++ NASA_prototype/src/lib/SceneInit.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import Stats from "three/examples/jsm/libs/stats.module.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
 
 export default class SceneInit {
   constructor(fov = 36, camera, scene, stats, controls, renderer) {
