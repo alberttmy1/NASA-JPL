8f60b20e5f82212a1d0b4413580db8165e568e1f
KevinFLoaiza <77510233+KevinFLoaiza@users.noreply.github.com>
2023-02-17 16:01:51 -0700

    Add files via upload
    test file for SPK

---
+++ spice notes/test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SpiceUsr.h"
+int main() {
+#define   STRLEN    37
+
+    SpiceChar      utc[STRLEN];
+    SpiceChar      obs[STRLEN];
+    SpiceChar      targ[STRLEN];
+    SpiceChar      kern[STRLEN];
+
+    SpiceDouble    tdb;
+    SpiceDouble    lt;
+    SpiceDouble    state[6];
+
+    prompt_c("Enter kernel file name > ", STRLEN, kern);
+    furnsh_c(kern);
+
+    prompt_c("Enter UTC epoch             > ", STRLEN, utc);
+    prompt_c("Enter observer name         > ", STRLEN, obs);
+    prompt_c("Enter target name           > ", STRLEN, targ);
+
+    str2et_c(utc, &tdb);
+
+    spkezr_c(targ, tdb, "J2000", "NONE", obs, state, &lt);
+
+    printf("\nEpoch               : %22.10f\n", tdb);
+    printf("   x-position   (km): %22.10f\n", state[0]);
+    printf("   y-position   (km): %22.10f\n", state[1]);
+    printf("   z-position   (km): %22.10f\n", state[2]);
+    printf("   x-velocity (km/s): %22.10f\n", state[3]);
+    printf("   y-velocity (km/s): %22.10f\n", state[4]);
+    printf("   z-velocity (km/s): %22.10f\n", state[5]);
+
+    //TODO: How to keep state[0], state[1], state[2] in a json file. 
+
+
+    return (0);
+
+
+}
