ba7dbc946c42ac94befc445b9fc97db894f6e6ab
makwayne117 <mak.wayne117@gmail.com>
2023-03-22 14:13:42 -0600

    api brief

--- NASA_prototype/python-bckend/positioningRefactored.py
+++ NASA_prototype/python-bckend/positioningRefactored.py
@@ -59,6 +59,38 @@ def return_position_form():
 
     return jsonify({"x": return_pos[0], "y": return_pos[1], "z": return_pos[2]}) #RETURN COORDINATES IN JSON.
 
+@app.route('/brief_parse', methods=['GET'])
+def brief_parse():
+    from subprocess import check_output
+    # print out summary of file
+    out = check_output(["brief", "jup068.bsp"])
+
+    # convert to string
+    c = str(out)
+    # add 7 to start the split to s
+    s = c.find("Bodies")
+    e = c.find("Start")
+
+    # slice the string
+    sp = c[s + 8:e]
+
+    # remove the n string
+    rn = sp.replace("n", "")
+
+    # remove the \
+    rs = rn.replace("\ ", "")
+
+    # split the string
+    f = rs.split()
+    # empty array
+    arr = []
+    # remove any barycenters and numbers
+    for x in range(len(f)):
+        if (f[x] != "BARYCENTER" and f[x][0] != "("):
+            arr.append(f[x])
+    print(arr)
+
+
 @app.route('/')
 def index():
     # A welcome message to test our server
