4822f93114f0021e7603d0cefe7e4a6b1e67a6a9
alberttmy1 <alta2479@colorado.edu>
2023-04-18 15:47:19 -0600

    stuff

--- NASA_prototype/src/lib/Planet.js
+++ NASA_prototype/src/lib/Planet.js
@@ -1,5 +1,10 @@
 import * as THREE from "three";
-
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+// import { TextGeometry} from "three";
+// import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
+import font_file from '../fonts/Bebas_Neue_Regular.json';
+import dro from 'three/examples/fonts/droid/droid_serif_regular.typeface.json';
 //pos is array of planet positions
 //check if pos[0] is equal to current days planet position
 //if not
@@ -18,7 +23,8 @@ export default class Planet {
     //orbit shit
     this.orbit = undefined;
     this.halo = this.createHalo();
-    this.createOrbit()
+    this.createOrbit();
+    // this.textName = this.displayName(name, this.toAU(positionX),this.toAU(positionY),this.toAU(positionZ));
   }
 
   toAU(km){
@@ -81,15 +87,17 @@ export default class Planet {
         orbitLine.setAttribute( 'position', new THREE.Float32BufferAttribute( this.pos.flat(), 3 ) );
         orbitLine.computeBoundingSphere();
         var line = new THREE.Line(orbitLine, material);
-
+        console.log("font:" + Object.values(font_file));
+        console.log("dro" + Object.values(dro));
         line.position.set(0,0,0);
         this.orbit = line;
-        //creates planet mesh, adds to solarsystem
+        //creates planet mesh, adds to solarsystems
         var planetMesh = this.getMesh();
         var system = new THREE.Group();
         system.add(planetMesh);
         system.add(this.orbit);
         system.add(this.halo);
+        // system.add(this.textName);
         this.screen.scene.add(system);
       });
   };
@@ -132,9 +140,98 @@ export default class Planet {
     line.position.set(this.pos[0][0], this.pos[0][1], this.pos[0][2]);
     return line;
   }
+  
   //will be used to display planet name near planet
-  /*displayName(){
+  displayName(name, x, y,z){
+    // const data = require('./fonts/Vogue_Regular.json');
+    // const fonts = fontAll(require.context('./font', false, /\.(json)$/));
+    //"../fonts/Vogue_Regular.json"
+    // ../fonts/Bebas_Neue_Regular.json'
+    // node_modules/three/examples/fonts/droid/droid_serif_regular.typeface.json
 
-  }*/
+
+    // Create a canvas element
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+
+    // Set canvas size
+    canvas.width = 6;
+    canvas.height = 10;
+
+    // Set text properties
+    context.font = '5px Arial';
+    context.fillStyle = 'white';
+    
+
+    // Draw text on canvas
+    context.fillText(name, canvas.width / 2, canvas.height / 2);
+
+    // Create a texture from the canvas
+    var texture = new THREE.CanvasTexture(canvas);
+
+    // Create a plane geometry to display the text
+    var geometry = new THREE.PlaneGeometry(6, 2);
+    var material = new THREE.MeshBasicMaterial({ map: texture });
+    var mesh = new THREE.Mesh(geometry, material);
+    
+    mesh.position.set(x, y, z);
+
+    // Add the mesh to the scene
+    return mesh;
+    
+    // const canvas = document.createElement('canvas');
+    // const context = canvas.getContext('2d');
+    // context.fillStyle = 'green';
+    // context.font = 'italic 5px Arial';
+    // // context.textAlign = 'center';
+    // context. textBaseline = 'middle';
+    // context.fillText(name, 0, 10);
+    // const texture = new THREE.Texture(canvas);
+    
+    // texture.needsUpdate = true;
+    // var material = new THREE.MeshBasicMaterial({
+    //   map: texture,
+    //   side: THREE.DoubleSide,
+    // })
+    // material.transparent = true
+    // var mesh = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), material)
+    // mesh.position.set(x, y, z);
+    // return mesh
+
+    // var fontLoader = new FontLoader();
+    // console.log("name");
+    // // console.log("../fonts/Bebas_Neue_Regular.json");
+    // fontLoader.load("three/examples/fonts/droid/droid_serif_regular.typeface.json", (font) => {
+    // // Create a TextGeometry using the loaded font
+    //   console.log("name");
+    //   const text = new TextGeometry(name, {
+    //     font: font,
+    //     size: 1,
+    //     height: 0.1,
+    //     });
+    //     const textMaterial = new THREE.MeshNormalMaterial();
+    //     const textMesh = new THREE.Mesh(text, textMaterial);
+    //     textMesh.position.x = -46;
+    //     textMesh.position.y = -10;
+    //     return textMesh;
+    // });
+
+    // const ttfLoader = new TTFLoader();
+    // ttfLoader.load("../fonts/jet_brains_mono_regular.ttf", (json) => {
+    //   // First parse the font.
+    //   const jetBrainsFont = fontLoader.parse(json);
+    //   // Use parsed font as normal.
+    //   const textGeometry = new TextGeometry(name, {
+    //     height: 2,
+    //     size: 10,
+    //     font: jetBrainsFont,
+    //   });
+    //   const textMaterial = new THREE.MeshNormalMaterial();
+    //   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    //   textMesh.position.x = -46;
+    //   textMesh.position.y = -10;
+    //   return textMesh;
+    // });
+  }
 
 }
