19b94d1b71511bbe536b19317d713e2932da7193
tigermcdaniel <tigermcdaniel@github.com>
2023-04-27 11:10:00 -0600

    missions menu cleanup

--- NASA_prototype/src/functions.js
+++ NASA_prototype/src/functions.js
@@ -161,7 +161,7 @@ export function addButtons(objects, id, pinned, planets) {
     checkbox.checked = true;
     var label = document.createElement("label");
     label.htmlFor = "env_traj";
-    label.innerHTML = " Show Trajectory";
+    label.innerHTML = " Show Orbit";
     content.appendChild(checkbox);
     content.appendChild(label);
     content.appendChild(document.createElement("br"));
@@ -197,4 +197,117 @@ export function addButtons(objects, id, pinned, planets) {
 
     i += 1;
   });
+}
+  // function to add buttons to collapsible
+  export function addMissions(objects) {
+
+    var buttonsContainer = document.getElementById('mission_library');
+    i = 0;
+    objects.forEach(function(objects) {
+      var button = document.createElement("button");
+      button.innerHTML = objects.replace(" BARYCENTER","");
+      button.type = "button";
+      button.classList.add("collapsible");
+
+      // create content div
+      var content = document.createElement("div");
+      content.classList.add("content");
+
+      // create pin checkbox
+      var pinCheckbox = document.createElement("input");
+      pinCheckbox.type = "checkbox";
+      pinCheckbox.id = "pin_checkbox";
+      pinCheckbox.checked = false;
+      pinCheckbox.classList.add("pinCheckbox");
+      pinCheckbox.addEventListener("click", function() {
+        var parent = button.parentNode;
+        var parent_content = content.parentNode;
+        if (this.checked) {
+          button.classList.add("pinned");
+        } else {
+          button.classList.remove("pinned");
+        }
+        if (this.checked){
+          // send it to the pinned list 
+          parent_content.removeChild(content);
+          parent.removeChild(button);
+
+          document.getElementById('pinned_missions').appendChild(button);
+          document.getElementById('pinned_missions').appendChild(content);
+        }else{
+          // send it to the library list 
+          parent_content.removeChild(content);
+          parent.removeChild(button);
+
+          document.getElementById('mission_library').appendChild(button);
+          document.getElementById('mission_library').appendChild(content);
+        }
+      });
+      var label = document.createElement("label");
+      label.htmlFor = "pin_checkbox";
+      label.innerHTML = " Pin";
+      content.appendChild(pinCheckbox);
+      content.appendChild(label);
+      content.appendChild(document.createElement("br"));
+
+      // add checkboxes to content div
+      // BODY
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = objects + "_body";
+      checkbox.value = "Body";
+      checkbox.name = "showBody";
+      checkbox.checked = true;
+      var label = document.createElement("label");
+      label.htmlFor = "env_body";
+      label.innerHTML = " Body";
+      content.appendChild(checkbox);
+      content.appendChild(label);
+      content.appendChild(document.createElement("br"));
+      
+      // TRAJECTORY
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = objects + "_traj";
+      checkbox.value = "Trajectory";
+      checkbox.name = "showTraj";
+      checkbox.checked = true;
+      var label = document.createElement("label");
+      label.htmlFor = "env_traj";
+      label.innerHTML = " Show Trajectory";
+      content.appendChild(checkbox);
+      content.appendChild(label);
+      content.appendChild(document.createElement("br"));
+      
+      // SPEED GRADIENT
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = objects + "_grad";
+      checkbox.value = "Gradient";
+      checkbox.name = "showGrab";
+      checkbox.checked = true;
+      var label = document.createElement("label");
+      label.htmlFor = "env_grad";
+      label.innerHTML = " Speed Gradient";
+      content.appendChild(checkbox);
+      content.appendChild(label);
+      content.appendChild(document.createElement("br"));
+
+      // add button and content to the document
+      buttonsContainer.appendChild(button);
+      buttonsContainer.appendChild(content);
+
+      // add event listener to button
+      button.addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+
+      i += 1;
+    });
 }
